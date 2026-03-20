177ac7ab2121fd32f11ae421ae85c20bc022e0e3
Jhon Araujo <tu-correo@ejemplo.com>
2026-03-19 22:35:02 -0400

    Actualización de archivos web

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.u29s0fc3tk"
+    "revision": "0.017kurupkno"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
